2a4606171013b7d9f07c8014ad44a8fba876d189
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-08 10:20:00 +0100

    Simplify the AppImage making script

--- gnome-text-editor-appimage.sh
+++ gnome-text-editor-appimage.sh
@@ -3,20 +3,18 @@
 set -eux
 
 ARCH="$(uname -m)"
-PACKAGE=gnome-text-editor
-URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
-SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 VERSION="$(cat ~/version)"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 
 # Variables used by quick-sharun
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export OUTNAME="$PACKAGE"-"$VERSION"-anylinux-"$ARCH".AppImage
+export OUTNAME=gnome-text-editor-"$VERSION"-anylinux-"$ARCH".AppImage
 export DESKTOP=/usr/share/applications/org.gnome.TextEditor.desktop
 export ICON=/usr/share/icons/hicolor/scalable/apps/org.gnome.TextEditor.svg
 export DEPLOY_OPENGL=1
 export STARTUPWMCLASS=gnome-text-editor # For Wayland, this is 'org.gnome.TextEditor', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
 
-# DEPLOY ALL LIBS
+# Trace and deploy all files and directories needed for the application (including binaries, libraries and others)
 wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
 ./quick-sharun /usr/bin/gnome-text-editor
@@ -28,18 +26,13 @@ for lang in $langs; do
   cp -vr /usr/share/help/$lang/gnome-text-editor/* ./AppDir/share/help/$lang/gnome-text-editor/
 done
 
-## Further debloat locale
-find ./AppDir/share/locale -type f ! -name '*glib*' ! -name '*gnome-text-editor*' -delete
-
 ## Set gsettings to save to keyfile, instead to dconf
 echo "GSETTINGS_BACKEND=keyfile" >> ./AppDir/.env
 
-# MAKE APPIMAGE WITH URUNTIME
-wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
-chmod +x ./uruntime2appimage
-./uruntime2appimage
+# Make the AppImage with uruntime
+./quick-sharun --make-appimage
 
-# PREPARE APPIMAGE FOR RELEASE
+# Prepare the AppImage for release
 mkdir -p ./dist
 mv -v ./*.AppImage* ./dist
 mv -v ~/version     ./dist
