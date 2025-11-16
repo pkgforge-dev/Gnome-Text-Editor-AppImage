bf7c47ee5d9df668952f69c4e6d4930e15a876f9
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-16 10:13:28 +0100

    Migrate to new generic AppImage template

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,34 +1,18 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
-DEBLOATED_PKGS_INSTALLER="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+ARCH=$(uname -m)
 
-echo "Installing build dependencies for sharun & AppImage integration..."
+echo "Installing build dependency expac..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-	base-devel \
-	curl \
-	desktop-file-utils \
-	git \
-	libxtst \
-	wget \
-	xorg-server-xvfb \
-	zsync \
-    expac
-echo "Installing the app & it's dependencies..."
+pacman -Syu --noconfirm expac
+
+echo "Installing package and its dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-	gnome-text-editor
-echo "Installing optional dependencies for 'enchant' spelling library automatically"
+pacman -Syu --noconfirm gnome-text-editor
 pacman -Syu --needed --noconfirm --asdeps $(expac -Ss '%o' enchant)
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$DEBLOATED_PKGS_INSTALLER" -O ./get-debloated-pkgs.sh
-chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh libxml2-mini mesa-nano gtk4-mini gdk-pixbuf2-mini librsvg-mini
-
-echo "Extracting the app version into a version file"
-echo "---------------------------------------------------------------"
-pacman -Q gnome-text-editor | awk '{print $2; exit}' > ~/version
+get-debloated-pkgs --add-common --prefer-nano
--- make-appimage.sh
+++ make-appimage.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
-ARCH="$(uname -m)"
-VERSION="$(cat ~/version)"
-SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
-
-# Variables used by quick-sharun
+ARCH=$(uname -m)
+VERSION=$(pacman -Q gnome-text-editor | awk '{print $2; exit}')
+export ARCH VERSION
+export OUTPATH=./dist
+export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export OUTNAME=gnome-text-editor-"$VERSION"-anylinux-"$ARCH".AppImage
-export DESKTOP=/usr/share/applications/org.gnome.TextEditor.desktop
 export ICON=/usr/share/icons/hicolor/scalable/apps/org.gnome.TextEditor.svg
+export DESKTOP=/usr/share/applications/org.gnome.TextEditor.desktop
 export DEPLOY_OPENGL=1
 export STARTUPWMCLASS=gnome-text-editor # For Wayland, this is 'org.gnome.TextEditor', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
 
 # Trace and deploy all files and directories needed for the application (including binaries, libraries and others)
-wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
-chmod +x ./quick-sharun
-./quick-sharun /usr/bin/gnome-text-editor
+quick-sharun /usr/bin/gnome-text-editor
 
 ## Copy help files for Help section to work
 langs=$(find /usr/share/help/*/gnome-text-editor/ -type f | awk -F'/' '{print $5}' | sort | uniq)
@@ -26,10 +23,5 @@ for lang in $langs; do
   cp -vr /usr/share/help/$lang/gnome-text-editor/* ./AppDir/share/help/$lang/gnome-text-editor/
 done
 
-# Make the AppImage with uruntime
-./quick-sharun --make-appimage
-
-# Prepare the AppImage for release
-mkdir -p ./dist
-mv -v ./*.AppImage* ./dist
-mv -v ~/version     ./dist
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
