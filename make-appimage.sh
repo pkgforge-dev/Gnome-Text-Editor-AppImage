836f9c987a6068d48caba008e3454def81662861
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-18 09:40:57 +0100

    Deploy help files directly in `quick-sharun`

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,14 +14,8 @@ export DEPLOY_OPENGL=1
 export STARTUPWMCLASS=gnome-text-editor # For Wayland, this is 'org.gnome.TextEditor', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
 
 # Trace and deploy all files and directories needed for the application (including binaries, libraries and others)
-quick-sharun /usr/bin/gnome-text-editor
-
-## Copy help files for Help section to work
-langs=$(find /usr/share/help/*/gnome-text-editor/ -type f | awk -F'/' '{print $5}' | sort | uniq)
-for lang in $langs; do
-  mkdir -p ./AppDir/share/help/$lang/gnome-text-editor/
-  cp -vr /usr/share/help/$lang/gnome-text-editor/* ./AppDir/share/help/$lang/gnome-text-editor/
-done
+quick-sharun /usr/bin/gnome-text-editor \
+             /usr/share/help/*/gnome-text-editor
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
